35e7ecd05d279ec916d633dc89dac40df3ec93f4
Daniel Altiparmak <daniel.altiparmak@tomcom.de>
2022-02-08 21:19:00 +0100

    extendes fish script

---
+++ functions/ssh_agent.fish
@@ -0,0 +1,97 @@
+set SSH_ENV "$HOME/.ssh/environment"
+
+function addsshkeys
+  set added_keys (ssh-add -l)
+  for key in (find ~/.ssh/ -not -name "*.pub" -a -iname "id_*")
+    if test ! (echo $added_keys | grep -o -e $key)
+      ssh-add "$key"
+    end
+  end
+end
+
+function start_agent
+    echo "Initialising new SSH agent..."
+    /usr/bin/ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+    echo succeeded
+    chmod 600 "$SSH_ENV"
+    . "$SSH_ENV" > /dev/null
+    addsshkeys
+end
+
+function sshagent_findsockets
+	find /tmp -uid (id -u) -type s -name agent.\* 2>/dev/null
+end
+
+function sshagent_testsocket
+    if [ ! -x (command which ssh-add) ] ;
+        echo "ssh-add is not available; agent testing aborted"
+        return 1
+    end
+
+    if [ X"$argv[1]" != X ] ;
+    	set -xg SSH_AUTH_SOCK $argv[1]
+    end
+
+    if [ X"$SSH_AUTH_SOCK" = X ]
+    	return 2
+    end
+
+    if [ -S $SSH_AUTH_SOCK ] ;
+        ssh-add -l > /dev/null
+        if [ $status = 2 ] ;
+            echo "Socket $SSH_AUTH_SOCK is dead!  Deleting!"
+            rm -f $SSH_AUTH_SOCK
+            return 4
+        else ;
+            echo "Found ssh-agent $SSH_AUTH_SOCK"
+            return 0
+        end
+    else ;
+        echo "$SSH_AUTH_SOCK is not a socket!"
+        return 3
+    end
+end
+
+
+function ssh_agent_init
+    # ssh agent sockets can be attached to a ssh daemon process or an
+    # ssh-agent process.
+
+    set -l AGENTFOUND 0
+
+    # Attempt to find and use the ssh-agent in the current environment
+    if sshagent_testsocket ;
+        set AGENTFOUND 1
+    end
+
+    # If there is no agent in the environment, search /tmp for
+    # possible agents to reuse before starting a fresh ssh-agent
+    # process.
+    if [ $AGENTFOUND = 0 ];
+        for agentsocket in (sshagent_findsockets)
+            if [ $AGENTFOUND != 0 ] ;
+	            break
+            end
+            if sshagent_testsocket $agentsocket ;
+	       set AGENTFOUND 1
+	    end
+
+        end
+    end
+
+    # If at this point we still haven't located an agent, it's time to
+    # start a new one
+    if [ $AGENTFOUND = 0 ] ;
+	    echo need to start a new agent
+	    start_agent
+      addsshkeys
+    end
+
+    # Finally, show what keys are currently in the agent
+    # ssh-add -l
+end
+
+# Source SSH settings, if they exist
+if status --is-interactive
+  ssh_agent_init
+end
